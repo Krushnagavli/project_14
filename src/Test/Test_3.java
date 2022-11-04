779b6c120562672f58e9bbfdc1ce13180e62a317
unknown <gavalikrushna2@gmail.com>
2022-11-04 22:43:10 +0530

    Test 3 added

--- src/Test/Test_3.java
+++ src/Test/Test_3.java
@@ -1,10 +1,11 @@
 package Test;
 
-public class Test_2 {
+public class Test_3 {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hi xyz");
+
+		System.out.println("Test 3");
 	}
 
 }
